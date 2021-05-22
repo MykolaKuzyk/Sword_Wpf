08ed82fb429207c7ffc39191f30c76bd7d28710f
Mykola Kuzyk <ukrboy99@gmail.com>
2021-05-23 00:22:29 +0200

    Changed main classes

--- Sword_Wpf/MainWindow.xaml.cs
+++ Sword_Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,19 +21,49 @@ namespace Sword_Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random random = new Random();
+        SwordDamage swordDamage = new SwordDamage();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            swordDamage.SetMagic(false);
+            swordDamage.SetFlamin(false);
+            
         }
 
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            swordDamage.SetFlamin(flaming.IsChecked.Value);
+            swordDamage.SetMagic(magic.IsChecked.Value);
+
+
+            swordDamage.CalculateDamage();
+            DisplayDamege();
+        }
+
+        public void DisplayDamege()
+        {
+            damage.Content = " Rolled " + swordDamage.Roll + " for " + swordDamage.Damage + " HP ";
+        }
         private void flaming_Checked(object sender, RoutedEventArgs e)
         {
-
+            swordDamage.SetFlamin(true);
+            DisplayDamege();
         }
 
         private void magic_Checked(object sender, RoutedEventArgs e)
         {
+            swordDamage.SetMagic(true);
+            DisplayDamege();
+               
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
         }
     }
 }
---
+++ Sword_Wpf/SwordDamage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text;
+
+//* To find the number of hit points (HP)
+//  of damage for a sword attack, roll 3d6 (three 6 - sided dice)
+//  and add "Base Damage" of 3HP
+//* Some swords are flaming, which causes an extra 2HP of damage
+//* Some swords are magic. For Magic swords, the 3D6 roll is multiplied by 1.75 
+//  and rounded down, and the base damage and flaming damage are added to the result 
+namespace Sword_Wpf
+{
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+
+        
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            Console.WriteLine("Rolled {0} for {1} HP  \n\n\n", Roll , Damage );
+
+            Debug.WriteLine($"CalculateDamage finished:  {Damage}(roll: {Roll})");
+        }
+
+// Set Sword as Magic sword 1 = yes , 3 = yes, 1 - no, 
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+           
+            Debug.WriteLine($"SetMagic finished:{Damage} (roll: {Roll})");
+        }
+
+
+        public void SetFlamin(bool isFlaming)
+        {
+            
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+            Debug.WriteLine($"SetFlamin finished:{Damage} (roll: {Roll}) is true?: {isFlaming}");
+        }
+    }
+}
